b5efb9268bcdbb70a24961a650d3d0afd2c0b4a7
Tom Marshall <tdm@cyngn.com>
2016-03-24 17:38:55 -0400

    mt2: AOKPify
    Change-Id: I9d0dbb34fbfb67a27f246e86cff3c7d812c6ecfe

--- aokp.mk
+++ aokp.mk
@@ -18,14 +18,14 @@ TARGET_SCREEN_WIDTH := 720
 
 $(call inherit-product, device/huawei/mt2/full_mt2.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/mt2/mt2.mk)
 
 PRODUCT_DEVICE := mt2
-PRODUCT_NAME := cm_mt2
+PRODUCT_NAME := aokp_mt2
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME=MT2L03 \
